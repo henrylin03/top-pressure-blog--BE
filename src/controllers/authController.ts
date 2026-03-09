59f265d7ee90655f03cb720b04d934b86d668f00
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-10 10:33:12 +1100

    feat(login): put placeholder middleware to confirm localstrategy authentication works
    add another middleware in array of controller functions _after_ passport.authenticate, which can be reached if successfully authenticated

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,9 @@
-// import type { Request, Response } from "express";
+import type { Request, Response } from "express";
 import passport from "@/lib/passport";
 
-const loginPost = passport.authenticate("local", { session: false });
+const loginPost = [
+	passport.authenticate("local", { session: false }),
+	(req: Request, res: Response) => res.json(req.user),
+];
 
 export { loginPost };
