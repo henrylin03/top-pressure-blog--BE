d084dd771531ffb455f552946af7c9a11a5e004c
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-10 20:14:06 +1100

    fix(confirmUserIsAuthorised): next() is called so that the actual core controller method can be called

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,15 +1,18 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import passport from "@/lib/passport";
 
 export const authenticateWithJwt = passport.authenticate("jwt", {
 	session: false,
 });
 
-export const confirmUserIsAuthorised = (req: Request, res: Response) => {
+export const confirmUserIsAuthorised = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	const { user } = req;
 	if (!user) return res.status(401).json({ error: "Unauthorised" });
 	if (!user.isAuthor)
 		return res.status(403).json({ error: "You must be an author to post" });
-
-	res.json(user);
+	next();
 };
