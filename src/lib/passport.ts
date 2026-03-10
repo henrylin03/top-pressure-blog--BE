13563cd86d62f72a71cd142fecb464c748ad9015
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-10 12:33:47 +1100

    refactor(passport): rename options object
    helps clear the namespace for options for jwt strategy

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -6,27 +6,30 @@ import {
 } from "passport-local";
 import { prisma } from "./prisma";
 
-const OPTIONS: IStrategyOptions = {
+const LOCAL_STRATEGY_OPTIONS: IStrategyOptions = {
 	usernameField: "usernameOrEmail",
 	session: false,
 };
 
 passport.use(
-	new LocalStrategy(OPTIONS, async (username, password, done) => {
-		try {
-			const user = await prisma.user.findFirst({
-				where: { OR: [{ email: username }, { username }] },
-			});
-			if (!user) return done(null, false);
+	new LocalStrategy(
+		LOCAL_STRATEGY_OPTIONS,
+		async (username, password, done) => {
+			try {
+				const user = await prisma.user.findFirst({
+					where: { OR: [{ email: username }, { username }] },
+				});
+				if (!user) return done(null, false);
 
-			const isMatchedPassword = await bcrypt.compare(password, user.password);
-			if (!isMatchedPassword) return done(null, false);
+				const isMatchedPassword = await bcrypt.compare(password, user.password);
+				if (!isMatchedPassword) return done(null, false);
 
-			return done(null, user);
-		} catch (error) {
-			done(error);
-		}
-	}),
+				return done(null, user);
+			} catch (error) {
+				done(error);
+			}
+		},
+	),
 );
 
 export default passport;
