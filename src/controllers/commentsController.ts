901cb0a9c06bd6e1beb26ef4ef676ce5dc82875d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-11 11:36:12 +1100

    feat(commentsController): throw 404 error back to client if post is not found in database. fix variable names

--- src/controllers/commentsController.ts
+++ src/controllers/commentsController.ts
@@ -3,14 +3,18 @@ import { prisma } from "@/lib/prisma";
 
 const getComments = async (req: Request, res: Response) => {
 	const { postId } = req.params;
-	if (!postId) return res.status(404).json({ error: "Post ID not found" });
+	const post = await prisma.post.findUnique({
+		where: { id: String(postId) },
+	});
+	if (!postId || !post)
+		return res.status(404).json({ error: "Post not found" });
 
-	const allPosts = await prisma.comment.findMany({
+	const comments = await prisma.comment.findMany({
 		where: { postId: String(postId) },
 	});
 	return res.status(200).json({
-		message: `${allPosts.length} comments retrieved for post`,
-		allPosts,
+		message: `${comments.length} comments retrieved for post`,
+		comments,
 	});
 };
 
